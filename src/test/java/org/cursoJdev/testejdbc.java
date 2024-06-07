456a3bc50948c5fc63b40742c5ed50560ba15b03
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-06-06 19:04:07 -0700

    finalizacao do banco de dados

--- src/test/java/org/cursoJdev/testejdbc.java
+++ src/test/java/org/cursoJdev/testejdbc.java
@@ -2,10 +2,11 @@ package org.cursoJdev;
 
 import conexaojdbc.SingleConnection;
 import junit.framework.TestCase;
+import org.junit.Test;
 
-public class testejdbc extends TestCase {
+public class testejdbc  {
 
-    @
+    @Test
     public void initBanco(){
         SingleConnection.getConnection();
     }
