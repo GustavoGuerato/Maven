22617c63f79737fd680ce24cc818466709d08467
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-06-10 13:38:27 -0700

    possibilidade de verificar os dados do banco de dados

--- src/test/java/org/cursoJdev/testejdbc.java
+++ src/test/java/org/cursoJdev/testejdbc.java
@@ -6,6 +6,7 @@ import model.UserPosJava;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class testejdbc  {
 
@@ -19,5 +20,20 @@ public class testejdbc  {
         userPosJava.setEmail("mateus@gmail.com");
 
         userPosDAO.salvar(userPosJava);
+    }
+
+    @Test
+    public void initListar() throws Exception {
+        UserPosDAO dao = new UserPosDAO();
+        try{
+            List<UserPosJava> list = dao.listar();
+            for (UserPosJava userPosJava : list){
+                System.out.println(userPosJava);
+                System.out.println("-----------------");
+            }
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
+
     }
 }
